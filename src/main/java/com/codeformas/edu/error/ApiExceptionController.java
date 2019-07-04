ce6bd767fc78fdc54246d53f341e6118049acc52
juanchoque <juanchoque1206@gmail.com>
2019-07-04 16:06:57 -0400

    feat: it was implemented exception manage

--- src/main/java/com/codeformas/edu/error/ApiExceptionController.java
+++ src/main/java/com/codeformas/edu/error/ApiExceptionController.java
@@ -1,14 +1,17 @@
 package com.codeformas.edu.error;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+@RestControllerAdvice
 public class ApiExceptionController {
-    @ExceptionHandler(value = ApiNotfoundException.class)
-    public ResponseEntity<Object> exception(ApiNotfoundException exception) {
-        return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+    @ExceptionHandler(value = ApiException.class)
+    public ResponseEntity<Object> exception(ApiException exception) {
+        ResponseEntity result = ResponseEntity.status(exception.getHttpStatus()).body(exception);
+        return result;
     }
 }
--- src/main/java/com/codeformas/edu/resources/AccountTypesResource.java
+++ src/main/java/com/codeformas/edu/resources/AccountTypesResource.java
@@ -1,18 +1,31 @@
 package com.codeformas.edu.resources;
 
-import com.codeformas.edu.error.ApiNotfoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeformas.edu.error.ApiException;
+import com.codeformas.edu.managers.AccountPypesEntityManager;
+import com.codeformas.edu.model.AccountPypesEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/accounttypes")
 public class AccountTypesResource {
+    private AccountPypesEntityManager accountPypesEntityManager;
 
-    @GetMapping("")
-    public ResponseBody getAllAccountsTypes(){
-        throw new ApiNotfoundException();
+    @Autowired
+    public AccountTypesResource(AccountPypesEntityManager accountPypesEntityManager) {
+        this.accountPypesEntityManager = accountPypesEntityManager;
+    }
+
+    @GetMapping(value = "", produces = "application/json")
+    public ResponseEntity getAllAccountsTypes(@RequestParam Optional<Integer> limit, @RequestParam Optional<Integer> offset){
+        List<AccountPypesEntity> resultList = this.accountPypesEntityManager.getAllAccountTypes(limit, offset);
+        ResponseEntity resultResponseEntity = ResponseEntity.status(HttpStatus.OK).body(resultList);
+        return resultResponseEntity;
     }
 
 }
